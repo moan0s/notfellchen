549fd33ccce3cf38d357db83cb8843cb0f14134d
moanos <julian-samuel@gebuehr.net>
2024-03-18 14:21:22 +0100

    feat(test): Add second adoption notice

--- src/fellchensammlung/management/commands/populate_db.py
+++ src/fellchensammlung/management/commands/populate_db.py
@@ -17,6 +17,9 @@ class Command(BaseCommand):
         rat2 = baker.make_recipe(
             'fellchensammlung.rat'
         )
+        cat = baker.make_recipe(
+            'fellchensammlung.cat'
+        )
         rescue1 = baker.make_recipe(
             'fellchensammlung.rescue_org'
         )
@@ -25,6 +28,8 @@ class Command(BaseCommand):
         )
 
         baker.make(AdoptionNotice, name="Vermittung1", animals=[rat1, rat2], organization=rescue1)
+
+        baker.make(AdoptionNotice, name="Vermittung2", animals=[cat], organization=rescue2)
 
         User.objects.create_user('test', password='foobar')
         User.objects.create_superuser(username="admin", password="admin")
