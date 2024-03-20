4964f1190322030e7fb06e9b038e50336a9fc369
moanos <julian-samuel@gebuehr.net>
2024-03-20 11:09:38 +0100

    feat: Add rules to admin, remove markdown content

--- src/fellchensammlung/admin.py
+++ src/fellchensammlung/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Animal, Species, RescueOrganization, AdoptionNotice, Location, MarkdownContent
+from .models import Animal, Species, RescueOrganization, AdoptionNotice, Location, Rule
 
 admin.site.register(Animal)
 admin.site.register(Species)
 admin.site.register(RescueOrganization)
 admin.site.register(Location)
 admin.site.register(AdoptionNotice)
-admin.site.register(MarkdownContent)
+admin.site.register(Rule)
