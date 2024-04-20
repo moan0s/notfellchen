6d1cf9084b3938b9a3d24035a717f81d2491c8f4
moanos <julian-samuel@gebuehr.net>
2024-04-20 07:48:48 +0200

    feat: Add crispy forms

--- src/notfellchen/settings.py
+++ src/notfellchen/settings.py
@@ -144,6 +144,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fontawesomefree',
+    'crispy_forms',
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -239,6 +241,9 @@ LANGUAGES = (
     ('en', _('English')),
     ('de', _('German')),
 )
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
