7deedad351bf16117d1c239abe4480a872075ff5
moanos <julian-samuel@gebuehr.net>
2024-08-08 17:17:00 +0200

    fix: month calculation

--- src/fellchensammlung/tools/misc.py
+++ src/fellchensammlung/tools/misc.py
@@ -15,6 +15,7 @@ def age_as_hr_string(age: datetime.timedelta) -> str:
     months = age.days/30
     years = age.days/365
     if years >= 1:
+        months = months - 12*years
         return f'{years:.0f} Jahr{pluralize(years)} und {months:.0f} Monat{pluralize(months)}'
     elif months >= 3:
         return f'{months:.0f} Monat{pluralize(months)}'
