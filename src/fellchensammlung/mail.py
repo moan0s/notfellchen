43f48e56592bb9695966c36fae8d5ee3c8b9af73
moanos <julian-samuel@gebuehr.net>
2024-09-15 09:00:27 +0200

    fix: Don't use backslash in f-strings
    https://stackoverflow.com/questions/66173070/how-to-put-backslash-escape-sequence-into-f-string

--- src/fellchensammlung/mail.py
+++ src/fellchensammlung/mail.py
@@ -8,21 +8,22 @@ from django.db.models import Q, Min
 from fellchensammlung.models import User
 from notfellchen.settings import host
 
+NEWLINE = "\r\n"
 
 def mail_admins_new_report(report):
     subject = _("Neue Meldung")
     for moderator in User.objects.filter(trust_level__gt=User.TRUST_LEVEL[User.MODERATOR]):
-        greeting = _("Moin,") + "\r\n"
-        new_report_text = _("es wurde ein Regelverstoß gemeldet.") + "\r\n"
+        greeting = _("Moin,") + "{NEWLINE}"
+        new_report_text = _("es wurde ein Regelverstoß gemeldet.") + "{NEWLINE}"
         if len(report.reported_broken_rules.all()) > 0:
-            reported_rules_text = (f"Ein Verstoß gegen die folgenden Regeln wurde gemeldet:\r\n"
-                                   f"- {'\r\n - '.join([str(r) for r in report.reported_broken_rules.all()])}\r\n")
+            reported_rules_text = (f"Ein Verstoß gegen die folgenden Regeln wurde gemeldet:{NEWLINE}"
+                                   f"- {f'{NEWLINE} - '.join([str(r) for r in report.reported_broken_rules.all()])}{NEWLINE}")
         else:
-            reported_rules_text = f"Es wurden keine Regeln angegeben gegen die Verstoßen wurde.\r\n"
+            reported_rules_text = f"Es wurden keine Regeln angegeben gegen die Verstoßen wurde.{NEWLINE}"
         if report.user_comment:
-            comment_text = f'Kommentar zum Report: "{report.user_comment}"\r\n'
+            comment_text = f'Kommentar zum Report: "{report.user_comment}"{NEWLINE}'
         else:
-            comment_text = f"Es wurde kein Kommentar hinzugefügt.\r\n"
+            comment_text = f"Es wurde kein Kommentar hinzugefügt.{NEWLINE}"
 
         report_url = "https://" + host + report.get_absolute_url()
         link_text = f"Um alle Details zu sehen, geh bitte auf: {report_url}"
