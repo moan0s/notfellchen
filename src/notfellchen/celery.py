f660a6b49aba043a4ebb705e7652ffb8fd0d02af
moanos <julian-samuel@gebuehr.net>
2024-10-10 22:26:28 +0200

    fix: varname

--- src/notfellchen/celery.py
+++ src/notfellchen/celery.py
@@ -22,7 +22,7 @@ app.conf.beat_schedule = {
     },
 }
 
-if settings.HEALTHCHECK_URL is not None and settings.HEALTHCHECK_URL != "":
+if settings.HEALTHCHECKS_URL is not None and settings.HEALTHCHECKS_URL != "":
     # If a healthcheck is configured, this will send an hourly ping to the healthchecks server
     app.conf.beat_schedule['hourly-healthcheck'] = {'task': 'tools.healthcheck',
                                                     'schedule': crontab(minute=32),
