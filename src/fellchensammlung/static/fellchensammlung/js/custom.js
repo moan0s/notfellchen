50897b6d35437a87d7af8b6cf01fd983c1031e3f
moanos <julian-samuel@gebuehr.net>
2025-01-09 23:39:23 +0100

    fix: remove double , when searching for city

--- src/fellchensammlung/static/fellchensammlung/js/custom.js
+++ src/fellchensammlung/static/fellchensammlung/js/custom.js
@@ -16,6 +16,6 @@ function geojson_to_summary(location) {
 }
 
 function geojson_to_searchable_string(location) {
-    return ifdef(location.properties.name, "", ", ") + ifdef(location.properties.street, "", ifdef(location.properties.housenumber, " ","")) + ifdef(location.properties.city, ", ", "") + ifdef(location.properties.country, ", ", "")
+    return ifdef(location.properties.name, "", ", ") + ifdef(location.properties.street, "", ifdef(location.properties.housenumber, " ",", ")) + ifdef(location.properties.city, "", ", ") + ifdef(location.properties.country, "", "")
 }
 
