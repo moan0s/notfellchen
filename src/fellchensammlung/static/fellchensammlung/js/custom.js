bc1f4e7ab7e586ae166a867d2851f71e052a8b67
moanos <julian-samuel@gebuehr.net>
2025-01-09 23:15:10 +0100

    feat: construct search results better

--- src/fellchensammlung/static/fellchensammlung/js/custom.js
+++ src/fellchensammlung/static/fellchensammlung/js/custom.js
@@ -4,5 +4,18 @@ function ifdef(variable, prefix = "", suffix = "") {
     } else {
         return "";
     }
+}
+
+function geojson_to_summary(location) {
+    if (ifdef(location.properties.name) !== "") {
+        return location.properties.name + ifdef(location.properties.city, " (", ")");
+    } else {
+        return ifdef(location.properties.street, "", ifdef(location.properties.housenumber, " ",""))  + ifdef(location.properties.city, ", ", "") + ifdef(location.properties.countrycode, ", ", "")
+    }
 
 }
+
+function geojson_to_searchable_string(location) {
+    return ifdef(location.properties.name, "", ", ") + ifdef(location.properties.street, "", ifdef(location.properties.housenumber, " ","")) + ifdef(location.properties.city, ", ", "") + ifdef(location.properties.country, ", ", "")
+}
+
