e11848ea72176e09a5098dc7be8519c32fa32002
moanos <julian-samuel@gebuehr.net>
2025-05-11 13:43:26 +0200

    feat: Add js to close notifications

--- src/fellchensammlung/static/fellchensammlung/js/toggles.js
+++ src/fellchensammlung/static/fellchensammlung/js/toggles.js
@@ -19,3 +19,14 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+// Looks for all notifications with a delete and allows closing them when pressing delete
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
