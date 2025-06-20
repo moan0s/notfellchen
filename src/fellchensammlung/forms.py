6f5e75a1b30eed4d22c8c76740854b32d13b08a1
moanos <julian-samuel@gebuehr.net>
2025-06-20 18:41:49 +0200

    refactor: change comment form to bulma

--- src/fellchensammlung/forms.py
+++ src/fellchensammlung/forms.py
@@ -106,13 +106,6 @@ class ReportCommentForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'form-comments'
-        self.helper.add_input(Hidden('action', 'comment'))
-        self.helper.add_input(Submit('submit', _('Kommentieren'), css_class="button is-primary"))
-
     class Meta:
         model = Comment
         fields = ('text',)
