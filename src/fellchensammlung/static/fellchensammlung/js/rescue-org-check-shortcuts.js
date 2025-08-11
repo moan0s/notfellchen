c93b2631cbf80a1bfa0a589e2416a0c4e934c2e1
moanos <julian-samuel@gebuehr.net>
2025-08-11 22:16:26 +0200

    feat: Add shortcut to open rescue org website

--- src/fellchensammlung/static/fellchensammlung/js/rescue-org-check-shortcuts.js
+++ src/fellchensammlung/static/fellchensammlung/js/rescue-org-check-shortcuts.js
@@ -2,4 +2,14 @@ function mark_checked(index) {
     document.getElementById('mark_checked_'+index).submit();
 }
 
+function open_information(index) {
+    let link = document.getElementById('species_url_'+index+'_1');
+    if (!link) {
+        link = document.getElementById('rescue_org_website_'+index);
+    }
+    window.open(link.href);
+}
+
 Mousetrap.bind('c', function() { mark_checked(1); });
+
+Mousetrap.bind('o', function() { open_information(1); });
